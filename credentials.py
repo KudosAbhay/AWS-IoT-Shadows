8211399703df2b2e570085fd49f5f66754f65345
KudosAbhay <kudosabhay@gmail.com>
2017-11-21 18:10:05 +0530

    deleted unnecessary files

--- credentials.py
+++
@@ -1,9 +0,0 @@
-# ==================================================================
-# CHANGE VALUES FOR FOLLOWING VARIABLES AS PER YOUR SETUP
-ACCESS_KEY = "Your Access Key" # Create one from AWS IAM Module
-SECRET_KEY = "Your Secret Key" # Create one from AWS IAM Module
-IOT_ENDPOINT = "Your IoT Endpoint" # From AWS IoT Dashboard, go to "settings" to find your IoT Endpoint
-AWS_REGION = "Your AWS Region" # Your AWS Region. Full list at - http://docs.aws.amazon.com/general/latest/gr/rande.html#iot_region
-HTTPS_ENDPOINT_URL = "Your Endpoint URL" # Prefix your AWS IoT Endpoint with "https://"
-IoT_THING_NAME = "Your Thing Name" # Put your AWS IoT Thing name here.
-# ==================================================================
